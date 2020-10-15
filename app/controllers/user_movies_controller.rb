cc69e9cd8b3baa8c5045fd45930de3a406929e9a
Hope <hopegiometti@gmail.com>
2020-10-15 10:28:12 -0600

    updated controllers

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,2 +1,23 @@
 class UserMoviesController < ApplicationController
+    def index
+        @user_movies = UserMovie.all
+        render json: @user_movies
+    end
+
+    def create
+        @user_movie = UserMovie.create(user_movie_params)
+        render json: @user_movie
+    end
+
+    def destroy
+        @user_movie = UserMovie.find(params[:id])
+        @user_movie.destroy
+        render json: { status: 'SUCCESS', message: 'deleted the post', data: @user_movie }
+    end
+
+    private 
+
+    def user_movie_params
+        params.permit(:user_id, :movie_id)
+    end
 end
